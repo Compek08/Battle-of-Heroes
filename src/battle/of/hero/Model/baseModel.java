24d429b1617e9f2098bb3af9518c1a3fb21c698f
Compek08 <38131748+Compek08@users.noreply.github.com>
2018-09-29 19:23:01 +0700

    Add files via upload

---
+++ src/battle/of/hero/Model/baseModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battle.of.hero.Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import Main.koneksi;
+
+/**
+ *
+ * @author rakas
+ */
+public abstract class baseModel {
+
+    private Connection koneksi;
+    private koneksi kon;
+
+    public baseModel() {
+        koneksi = new koneksi().getKoneksi();
+        kon = new koneksi();
+    }
+
+    abstract String login();
+
+    protected boolean insert(String query) throws SQLException {
+        try {
+            PreparedStatement st = koneksi.prepareStatement(query);
+            st.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected boolean update(String query) throws SQLException {
+        try {
+//            PreparedStatement st = koneksi.prepareStatement(query);
+//            st.executeQuery();
+            kon.execute(query);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected boolean delete(String query) throws SQLException {
+        try {
+            PreparedStatement st = koneksi.prepareStatement(query);
+            st.executeQuery();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
